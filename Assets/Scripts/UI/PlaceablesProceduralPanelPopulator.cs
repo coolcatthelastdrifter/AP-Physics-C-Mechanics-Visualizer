2ac38bea07e1d694b9d97d257f2ed411dd3daf58
coolcatthelastdrifter <ninjacatdragon@gmail.com>
2026-08-15 10:26:36 -0700

    Finish Build Panel Populator

--- Assets/Scripts/UI/PlaceablesProceduralPanelPopulator.cs
+++ Assets/Scripts/UI/PlaceablesProceduralPanelPopulator.cs
@@ -1,11 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlaceablesProceduralPanelPopulator : MonoBehaviour
 {
+    public GameObject exampleGridElement;
     void Start()
     {
-        
+        foreach (var(key, value) in PlaceablesDatabase.Instance.PlaceableSOs)
+        {
+            GameObject clone = Instantiate(exampleGridElement);
+            clone.transform.SetParent(exampleGridElement.transform.parent);
+
+            clone.name = key;
+            clone.transform.Find("Image").GetComponent<Image>().sprite = value.icon;
+            clone.transform.Find("NameHolder").transform.Find("Name").GetComponent<TextMeshProUGUI>().text = key;
+        }
+
+        Destroy(exampleGridElement);
     }
 }
